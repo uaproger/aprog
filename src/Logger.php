383aff5c1f5cdcbef136ca750af9f900350b3cb4
Oleksandr <zadorozhniy3@icloud.com>
2025-03-04 17:56:38 +0200

    logger fix 4

--- src/Logger.php
+++ src/Logger.php
@@ -8,9 +8,9 @@ class Logger
     const string GREEN = "\033[0;32m";
     const string YELLOW = "\033[0;33m";
     const string BLUE = "\033[0;34m";
-    const string BACKGROUND_BLUE = "\033[48;2;0;0;255m";   // Синій фон
-    const string BACKGROUND_RED = "\033[48;2;255;0;0m";    // Червоний фон
-    const string BACKGROUND_YELLOW = "\033[48;2;255;255;0m"; // Жовтий фон
+    const string BACKGROUND_ORANGE = "\033[48;2;255;136;0m";   // #FF8800 (Жовто-оранжевий)
+    const string BACKGROUND_RED = "\033[48;2;255;40;0m";      // #FF2800 (Червоний)
+    const string BACKGROUND_DARK_BLUE = "\033[48;2;4;52;88m"; // #043458 (Темно-синій)
     const string BOLD = "\033[1m";
     const string NC = "\033[0m";
 
@@ -31,16 +31,16 @@ class Logger
 
     public static function info($message): string
     {
-        return sprintf("%s" . self::BACKGROUND_BLUE . " INFO " . self::NC . "%s.", '  ', $message);
+        return sprintf("%s" . self::BACKGROUND_DARK_BLUE . " INFO " . self::NC . " %s.", '  ', $message);
     }
 
     public static function warn($message): string
     {
-        return sprintf("%s" . self::BACKGROUND_YELLOW . " WARN " . self::NC . "%s.", '  ', $message);
+        return sprintf("%s" . self::BACKGROUND_ORANGE . " WARN " . self::NC . " %s.", '  ', $message);
     }
 
     public static function error($message): string
     {
-        return sprintf("%s" . self::BACKGROUND_RED . " ERROR " . self::NC . "%s.", '  ', $message);
+        return sprintf("%s" . self::BACKGROUND_RED . " ERROR " . self::NC . " %s.", '  ', $message);
     }
 }
